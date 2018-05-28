900df2ddc305c35589d6a9303cb948938628dcda
leolinf <funlin.py@foxmail.com>
2018-05-28 14:47:50 +0800

    修改aiohttp

--- concurrent/aiohttp_demo.py
+++ concurrent/aiohttp_demo.py
@@ -8,21 +8,24 @@ loop = asyncio.get_event_loop()
 
 
 async def fetch(session, url):
-    with async_timeout.timeout(10):
-        async with session.get(url) as response:
-            a = await response.read()
-            try:
-                return await response.text()
-            except:
-                print( await response.content)
+#    with async_timeout.timeout(10):
+    async with session.get(url, timeout=1) as response:
+        print(dict(response.headers))
+        return await response.text()
 
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        return await fetch(session, 'http://www.baidu.com')
+        a = await fetch(session, 'https://www.baidu.com')
+        print(threading.currentThread())
+
+async def main1():
+    async with aiohttp.ClientSession() as session:
+        a = await fetch(session, 'https://www.python.org')
+        print(threading.currentThread())
 
 
 if __name__ == '__main__':
-    tasks = [main() for i in range(10)]
+    tasks = [main(), main1()]
     loop.run_until_complete(asyncio.gather(*tasks))
     print(loop)
