6cb23a1f4a254edf3eaef3ff81598b5c37bd8589
leolinf <funlin.py@foxmail.com>
2020-03-24 21:26:48 +0800

    quick sort

--- tools/math/insertion_sort.py
+++ tools/math/insertion_sort.py
@@ -15,7 +15,7 @@ print(arr)
 
 
 def insertSort(arr):
-    """快速排序"""
+    """插入排序"""
     for i in range(len(arr)):
         for j in range(i, 0, -1):
             # 最后一个数字肯定最大
@@ -32,7 +32,7 @@ def selectionSort(arr):
     """选择排序"""
     length = len(arr)
     for i in range(length):
-        #  选择最小索引
+        # 选择最小索引
         min_index = i
         for j in range(i, length):
             if arr[j] < arr[min_index]:
@@ -43,4 +43,57 @@ def selectionSort(arr):
 
 arr = [1, 2, 3, 123, 234, 23434, 345, 45, 234435, 566]
 selectionSort(arr)
+print(arr)
+
+
+def mergeSort(arr):
+    """归并排序，主要是采用分治法，和递归来处理"""
+    if len(arr) < 2:
+        return arr
+    middle = len(arr) // 2
+    left, right = arr[:middle], arr[middle:]
+
+    return merge(mergeSort(left), mergeSort(right))
+
+
+def merge(left, right):
+    result = []
+    while left and right:
+        if left[0] >= right[0]:
+            result.append(right.pop(0))
+        else:
+            result.append(left.pop(0))
+    while left:
+        result.append(left.pop(0))
+    while right:
+        result.append(right.pop(0))
+    return result
+
+
+arr = [12, 234, 1, 23, 5, 67, 76, 878]
+a = mergeSort(arr)
+print(a)
+
+
+def quickSort(arr, left, right):
+    """快速排序"""
+    if left >= right:
+        return
+    low = left
+    high = right
+    key = arr[low]
+    while left < right:
+        while left < right and arr[right] > key:
+            right -= 1
+        arr[left] = arr[right]
+        while left < right and arr[left] <= key:
+            left += 1
+        arr[right] = arr[left]
+    arr[right] = key
+    quickSort(arr, low, left - 1)
+    quickSort(arr, left + 1, high)
+
+
+arr = [123, 12, 4, 34234, 345, 65, 934, 23]
+quickSort(arr, 0, len(arr)-1)
 print(arr)
